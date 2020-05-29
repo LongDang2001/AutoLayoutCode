b1dde414b26d7037f3becb1f61653b5fca726204
LongDang2001 <62790457+LongDang2001@users.noreply.github.com>
2020-05-29 13:16:06 +0700

    first commit

---
+++ AutoLayoutCode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AutoLayoutCode
+//
+//  Created by admin on 4/3/20.
+//  Copyright © 2020 Long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var imageView = UIImageView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func imageView1() {
+        
+    }
+    
+    
+
+
+}
+
